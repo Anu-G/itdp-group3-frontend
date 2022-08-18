c95c5621ed578085391553cd308026bbbfb0b723
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-18 18:32:05 +0700

    [ADD] logo toktok di navbar

--- src/shared/components/ComponentNavbar.js
+++ src/shared/components/ComponentNavbar.js
@@ -1,16 +1,18 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Component.css'
 
 function ComponentNavbar() {
   return (
-    <>
-    <div>
-        <Link to="#" className='menu-bars'>
-            <FontAwesomeIcon icon="fa-solid fa-bars" /> 
-        </Link>
-    </div>
-    </>
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+      <img src="/logo-toktok.png" width="185.08" height="60" className="d-inline-block align-top" alt="logo-toktok"/>
+      </a>
+      <button className="btn btn-warning btn-circle" type="submit">
+      <FontAwesomeIcon icon="fa-solid fa-user" />
+      </button>
+    </nav>
   )
 }
 
-export default ComponentNavbar
+export default ComponentNavbar;
