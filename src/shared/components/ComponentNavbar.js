507397923c070ab2f1d12044fb52e2b381adf506
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-18 13:50:16 +0700

    [ADD] navbar component

---
+++ src/shared/components/ComponentNavbar.js
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+function ComponentNavbar() {
+  return (
+    <>
+    <div>
+        <Link to="#" className='menu-bars'>
+            <FontAwesomeIcon icon="fa-solid fa-bars" /> 
+        </Link>
+    </div>
+    </>
+  )
+}
+
+export default ComponentNavbar
