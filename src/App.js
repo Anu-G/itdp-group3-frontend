047f02319cffc83855543440171ca6585c968e7b
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-22 17:43:42 +0700

    [FIX] navbar sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter} from "react-router-dom";
 import { CategoryLabel } from './shared/components/CategoryLabel/CategoryLabel';
 import { CheckBox } from './shared/components/CheckBox/CheckBox';
 import { CommentColomn } from './shared/components/CommentColomn/CommentColomn';
 import { CustomDropdown } from './shared/components/Dropdown/Dropdown';
+import Navbar from './shared/components/Navbar/Navbar';
 
 const App = _ => {
   return (
     <div className='app'>
-      <CommentColomn/>
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
     </div>
   )
 }
