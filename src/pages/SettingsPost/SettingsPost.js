3ff4dd41789c78077f18b9b26bc98b9611cd2f09
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-24 16:34:54 +0700

    [ADD] settings post view

---
+++ src/pages/SettingsPost/SettingsPost.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SettingsPost.css';
+
+export const SettingsPost = () => {
+  return (
+    <div className='wrapper'>
+        <div className='settings-post-card'>
+            <div className='post-img'>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+            </div>
+
+            <div className='post-img'>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+                <div className='post-img-card'>
+                    <img src=''/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
