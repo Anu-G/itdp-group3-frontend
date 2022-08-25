80465bba164cd22054a0c79d8e1103671f46e56d
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-25 15:29:06 +0700

    [ADD] settings faq view

---
+++ src/pages/SettingsFaq/SettingsFaq.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ButtonComponent } from '../../shared/components/Button/Button'
+import { Text32White } from '../../shared/components/Label/Label'
+import { QA } from '../../shared/components/QA/QA'
+import'./SettingsFaq.css'
+
+export const SettingsFaq = ({question, questionAmount}) => {
+  return (
+    <div className='wrapper'>
+      <div className='settings-faq-card'>
+        <div className='button-add-faq-question-amount'>
+          <div className='button-add-question'>
+            <ButtonComponent label={"Add Question"}/>
+          </div>
+          <Text32White text={`${question}. ${questionAmount}`}/>
+        </div>
+
+        <div className='QA'>
+        <QA/>
+        <QA/>
+        <QA/>
+        </div>
+      </div>
+    </div>
+  )
+}
