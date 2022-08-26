0548b5f1d8cb4db7dfe04c0d51e3955ad7c26857
Barkah Niti Wisisto <barkahnitiwisisto@gmail.com>
2022-08-26 14:58:16 +0700

    [ADD] api service account

--- src/shared/factory/ServiceFactory.js
+++ src/shared/factory/ServiceFactory.js
@@ -1,7 +1,9 @@
 import AuthService from "../../services/AuthService";
+import SettingAccountService from "../../services/SettingAccountService";
 
 const ServiceFactory = (apiClient) => ({
-   authService: AuthService(apiClient)
+   authService: AuthService(apiClient),
+   settingAccountService : SettingAccountService(apiClient)
 });
 
 export default ServiceFactory;
