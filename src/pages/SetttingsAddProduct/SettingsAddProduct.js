66142b57c651d553fbdce7d41a93a4399551e59d
astikaist <astikaistiningrum@student.uns.ac.id>
2022-08-30 12:25:31 +0700

    [ADD] add product api

--- src/pages/SetttingsAddProduct/SettingsAddProduct.js
+++ src/pages/SetttingsAddProduct/SettingsAddProduct.js
@@ -1,76 +1,120 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import { UseDep } from '../../shared/context/ContextDep';
 import { ProgressBar } from 'react-bootstrap'
 import { ButtonComponent } from '../../shared/components/Button/Button'
-import { CommentColomn } from '../../shared/components/CommentColomn/CommentColomn'
-import { InputTextLabelLg, InputTextLabelMd, InputTextLabelSm } from '../../shared/components/InputWithLabel/InputWithLabel'
+import { InputTextLabelSm } from '../../shared/components/InputWithLabel/InputWithLabel'
 import { Title2Yellow, Title3White } from '../../shared/components/Label/Label'
 import './SettingsAddProduct.css'
 import _ from 'lodash';
+import { useSelector } from 'react-redux';
+import { AuthSelector } from '../../shared/selectors/Selectors';
+import AppError from '../../utils/AppError';
+import { BioColomn } from '../../shared/components/BioColomn/BioColomn'
 
 export const SettingsAddProduct = (props) => {
     const maxLength = 280
 
-    const [isActive, setIsActive] = useState(true)
-    const [description, setDescription] = useState('')
-    const [isButtonUploadActive, setIsButtonUploadActive] = useState(false)
+    let { id, label } = props;
+    const inputRef = useRef();
+    const [image, setImage] = useState(null);
+    const triggerFileSelectPopup = () => inputRef.current.click();
 
-    useEffect(() => {
-        if (description.length == 0) {
-            setIsButtonUploadActive(false)
-        } else if (description.length > maxLength) {
-            setIsButtonUploadActive(false)
-        } else {
-            setIsButtonUploadActive(true)
-        }
-    }, [description])
-
-    const handleDescriptionOnClick = () => {
-        setIsActive(!isActive)
-    }
-
-    const handleDescriptionChange = (event) => {
-        setDescription(event.target.value)
-    }
-
-    const handleOnClickUpload = () => {
-        console.log('ceritanya send')
-    }
-
-    let { id, label, uploadUrl } = props;
-    const [isUploding, setUploding] = useState(false);
-    const [uploadedImgs, setUplodedImgs] = useState([]);
-    const [uploadProgress, setProgress] = useState(0);
-
-    const handleChange = async e => {
-        let { files } = e.target;
-
-        let formData = new FormData();
-        _.forEach(files, file => {
-            formData.append('files', file);
+    const onSelectFile = (event) => {
+        if (event.target.files && event.target.files.length > 0) {
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.addEventListener("load", () => {
+            setImage(reader.result);
         });
+        }
+    };
 
-        // setUploding(true);
-        // let { data } = await API.post(uploadUrl, formData, {
-        //     onUploadProgress: ({ loaded, total }) => {
-        //         let progress = ((loaded / total) * 100).toFixed(2);
-        //         setProgress(progress);
-        //     }
-        // });
-        // setUplodedImgs(data);
-        // setUploding(false);
-    }
 
+    const [formData, setFormData] = useState({
+        productName:"",
+        price:"",
+        description:""
+     });
+
+     const [charLength, setCharLength] = useState(0);
+
+    const [result, setResult] = useState(null);
+     const productImageData = new FormData();
+     const { productImageService, productService } = UseDep();
+     const authRed = useSelector(AuthSelector);
+  
+     const saveResponse = async _ => {
+        let file = await fetch(result).then(r => r.blob()).then(blobFile => new File([blobFile], "imageProduct.jpg", { type: "image/png" }));
+        productImageData.append("detail_media_products", file);
+  
+        try {
+           const responseImage = await productImageService.doPostProductImage(productImageData);
+           if (responseImage.status === 200) {
+              try {
+                 const response = await productService.doPostProductData({
+                    account_id: `${authRed.account_id}`,
+                    product_name: formData.productName,
+                    price: formData.price,
+                    description: formData.description,
+                    detail_media_products: [responseImage.data.data]
+                 });
+                 if (response.status === 200) {
+                    alert('success');
+                 }
+              } catch (err) {
+                 AppError(err);
+              }
+           }
+        } catch (err) {
+           AppError(err);
+        }
+     }
+  
+     const onChangeProductName = (e) => {
+        setFormData(prevState => ({
+           ...prevState,
+           productName: e.target.value
+        }))
+     }
+  
+    //  useEffect(_ => {
+    //     setFormData(prevState => ({
+    //        ...prevState,
+    //        productName: authRed.productName
+    //     }));
+    //  }, []);
+  
+     const onChangePrice = (event) => {
+        setFormData(prevState => ({
+           ...prevState,
+           price: event.target.value
+        }))
+     }
+
+     const onChangeDescription = (event) => {
+        setFormData(prevState => ({
+           ...prevState,
+           description: event.target.value
+        }))
+        setCharLength(event.target.value.length)
+     }
+  
+     const charLimitHandle = (e) => {
+        if (charLength >= maxLength) {
+           e.preventDefault();
+        }
+     }
     return (
         <div className='wrapper'>
             <Title2Yellow title={"New Product"} />
             <div className='settings-add-product-card'>
                 <div className='name-price'>
-                    <InputTextLabelSm label={'Name'} style={{ minWidth: '254px' }} />
-                    <InputTextLabelSm label={'Price'} style={{ minWidth: '254px' }} />
+                    <InputTextLabelSm label={'Name'} id={"productName"} value={formData.productName} handleOnChange={onChangeProductName} style={{ minWidth: '254px' }} />
+                    <InputTextLabelSm label={'Price'} id={"price"} value={formData.price} handleOnChange={onChangePrice} style={{ minWidth: '254px' }} />
                 </div>
 
                 <div className='description'>
-                    <CommentColomn label='Description' handleChange={handleDescriptionChange} maxLength={maxLength} value={description} />
+                    <BioColomn label={'Description'} maxLength={maxLength} charLength={charLength} handleChange={onChangeDescription} charLimitHandle={charLimitHandle} value={formData.description}/>
                 </div>
 
                 <div className='add-photo-video'>
@@ -79,31 +123,15 @@ export const SettingsAddProduct = (props) => {
                         <label htmlFor={id} className="text-primary font-weight-bold">{label}</label>
                         <div className="d-flex">
                             <div className="d-flex">
-                                <input multiple className="file-input" type="file" id={id} onChange={handleChange} />
+                                <input multiple className='file-input' type="file" accept='image/*' ref={inputRef} style={{ display: "none" }} onChange={onSelectFile} name="fileName"  />
+                                <button onClick={triggerFileSelectPopup} style={{ borderRadius: "8px" }}>Choose Image</button>
                             </div>
-                            {/* {
-                              isUploding ? (
-                                  <div className="flex-grow-1 px-2">
-                                      <div className="text-center">{uploadProgress}%</div>
-                                      <ProgressBar value={uploadProgress} />
-                                  </div>
-                              ) : null
-                          } */}
-                        </div>
-                        <div className="d-flex flex-wrap mt-4">
-                            {
-                                uploadedImgs && !isUploding ? (
-                                    uploadedImgs.map(uploadedImg => (
-                                        <img src={uploadedImg} key={uploadedImg} alt="UploadedImage" className="img-thumbnail img-fluid uploaded-img mr-2" />
-                                    ))
-                                ) : null
-                            }
                         </div>
                     </div>
                 </div>
 
                 <div className='button-upload'>
-                    <ButtonComponent isDisable={isButtonUploadActive} label={'Upload'} onClick={handleOnClickUpload} />
+                    <ButtonComponent label={'Upload'} onClick={saveResponse} />
                 </div>
             </div>
         </div>
--- src/shared/factory/ServiceFactory.js
+++ src/shared/factory/ServiceFactory.js
@@ -2,18 +2,20 @@ import AuthService from "../../services/AuthService";
 import { CategoryService } from "../../services/CategoryService";
 import { ProfileService, ProfileImageService } from "../../services/ProfileService";
 import SettingAccountService from "../../services/SettingAccountService";
-import ProductService from "../../services/ProductService";
-import { PostImageService } from "../../services/PostService";
+import ProductService, { ProductImageService } from "../../services/ProductService";
+import { PostImageService, PostService } from "../../services/PostService";
 
 export const ServiceFactory = (apiClient) => ({
    authService: AuthService(apiClient),
    profileService: ProfileService(apiClient),
    settingAccountService: SettingAccountService(apiClient),
    categoryService: CategoryService(apiClient),
-   productService: ProductService(apiClient)
+   productService: ProductService(apiClient),
+   postService: PostService(apiClient)
 });
 
 export const ServiceImageFactory = (apiClient) => ({
    profileImageService: ProfileImageService(apiClient),
-   postImageService: PostImageService(apiClient)
+   postImageService: PostImageService(apiClient),
+   productImageService: ProductImageService(apiClient)
 });
