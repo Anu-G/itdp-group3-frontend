03b7a72541c15088b28ee7cd146907990b723707
nugroho.fitrianto <mnugrohofitrianto@gmail.com>
2022-09-28 23:06:18 +0700

    [FIX] app error utils

--- src/utils/AppErrors.js
+++ src/utils/AppErrors.js
@@ -1,30 +1,28 @@
 const AppError = (err) => {
-   if (err.response.status !== 400) {
-      if (err.response.status === 401) {
+   if (err?.response?.status !== 400) {
+      if (err?.response?.status === 401) {
          return 'session expired, please re-login';
       } else {
-         return (err.response.data);
+         return (err?.response?.data);
       }
    } else {
-      return (err.response.data.responseMessage);
+      return (err?.response?.data?.responseMessage);
    }
 }
 
 export const AppErrorAuth = (err) => {
-   if (err.response.status === 401) {
+   if (err?.response?.status === 401) {
       return 'session expired, please re-login';
    } else {
-      console.error(err);
       return false;
    }
 }
 
 export const AppErrorNoProfile = (err) => {
-   if (err.response.data.responseCode === 'X01' &&
-      err.response.data.responseMessage === 'record not found') {
+   if (err?.response?.data?.responseCode === 'X01' &&
+      err?.response?.data?.responseMessage === 'record not found') {
       return 'please complete your profile data first'
    } else {
-      console.error(err);
       return false;
    }
 }
