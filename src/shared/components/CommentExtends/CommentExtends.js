950a701813a7e0af86b3a79d4e046200a069d1a9
astika istiningrum <astikaistiningrum23@gmail.com>
2022-09-30 14:03:40 +0700

    [FIX] add profile in comment

--- src/shared/components/CommentExtends/CommentExtends.js
+++ src/shared/components/CommentExtends/CommentExtends.js
@@ -2,6 +2,7 @@ import './CommentExtends.css'
 
 import React from 'react'
 import { Text32White, Title3White } from '../Label/Label'
+import { AvatarSmall } from '../Avatar/Avatar'
 
 export const CommentExtends = ({comments}) => {
     
@@ -10,7 +11,7 @@ export const CommentExtends = ({comments}) => {
 
         {comments.map(comment => {
             return(
-                <DetailComment comment={comment.comment_fill} user={comment.user} key={comment.key}/>
+                <DetailComment comment={comment.comment_fill} user={comment.user} key={comment.key} profile={comment.avatar}/>
             )
         })}
     </div>
@@ -18,13 +19,19 @@ export const CommentExtends = ({comments}) => {
 
 }
 
-const DetailComment = ({user, comment}) => {
+const DetailComment = ({avatar, user, comment, accId, accountType, handleClickName}) => {
     return(
-        <div className='comment-ext-hd-wrp'>
-            <Title3White title={user}/>
-            <div style={{paddingLeft: '1rem'}}>
-                <Text32White text={comment}/>
+        <div className='avatar-comment-wrp'>
+            <div className='avatar-wrp'>
+                <AvatarSmall link={avatar} accId={accId} accType={accountType} handleClick={handleClickName}/>
+            </div>
+            <div className='comment-ext-hd-wrp'>
+                <Title3White title={user}/>
+                <div style={{paddingLeft: '1rem'}}>
+                    <Text32White text={comment}/>
+                </div>
             </div>
         </div>
+        
     )
 }
